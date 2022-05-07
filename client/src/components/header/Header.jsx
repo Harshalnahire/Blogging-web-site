6b558a7caa366daf3c35bb57b35e5597e4e0b76a
suyog2001 <suyog0057@gmail.com>
2022-05-07 14:38:41 +0530

    frontEnd with ui issue

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,50 @@
 import "./header.css";
-
+import Carousel from "react-bootstrap/Carousel";
+import carouselImage1 from "./../../assets/Carousel-1.jpg";
+import carouselImage2 from "./../../assets/Carousel-2.jpg";
 export default function Header() {
   return (
-    <div className="header">
-      <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">Blog</span>
-      </div>
-      <img
-        className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt=""
-      />
+    <div>
+     <div className="container">
+     <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-50"
+            // src={carouselImage1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-50"
+            // src={carouselImage2}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-50"
+            // src={carouselImage1}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+     </div>
     </div>
   );
 }
--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,30 +1,38 @@
-import { useEffect, useState } from "react";
-import Header from "../../components/header/Header";
-import "./About.css";
+import "./about.css";
 
 export default function About() {
   return (
     <>
-      <Header />
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
       <div className="home">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-        consequatur repudiandae? Provident explicabo architecto at
-        reprehenderit, sed illo eos asperiores fugiat inventore voluptatibus,
-        repellendus, similique quod nam neque placeat ab. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Illum, consequatur repudiandae?
-        Provident explicabo architecto at reprehenderit, sed illo eos asperiores
-        fugiat inventore voluptatibus, repellendus, similique quod nam neque
-        placeat ab. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Illum, consequatur repudiandae? Provident explicabo architecto at
-        reprehenderit, sed illo eos asperiores fugiat inventore voluptatibus,
-        repellendus, similique quod nam neque placeat ab. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Illum, consequatur repudiandae?
-        Provident explicabo architecto at reprehenderit, sed illo eos asperiores
-        fugiat inventore voluptatibus, repellendus, similique quod nam neque
-        placeat ab. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Illum, consequatur repudiandae? Provident explicabo architecto at
-        reprehenderit, sed illo eos asperiores fugiat inventore voluptatibus,
-        repellendus, similique quod nam neque placeat ab.
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+          consequatur repudiandae? Provident explicabo architecto at
+          reprehenderit, sed illo eos asperiores fugiat inventore voluptatibus,
+          repellendus, similique quod nam neque placeat ab. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Illum, consequatur repudiandae?
+          Provident explicabo architecto at reprehenderit, sed illo eos
+          asperiores fugiat inventore voluptatibus, repellendus, similique quod
+          nam neque placeat ab. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Illum, consequatur repudiandae? Provident explicabo
+          architecto at reprehenderit, sed illo eos asperiores fugiat inventore
+          voluptatibus, repellendus, similique quod nam neque placeat ab. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Illum, consequatur
+          repudiandae? Provident explicabo architecto at reprehenderit, sed illo
+          eos asperiores fugiat inventore voluptatibus, repellendus, similique
+          quod nam neque placeat ab. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Illum, consequatur repudiandae? Provident explicabo
+          architecto at reprehenderit, sed illo eos asperiores fugiat inventore
+          voluptatibus, repellendus, similique quod nam neque placeat ab.
+        </p>
       </div>
     </>
   );
